2fafc648d4472dccfb60eddeb066a63f8332010a
Mylena Mahatma <104845006+mylenamahatma@users.noreply.github.com>
2025-09-05 23:41:35 -0300

    Fix: Alteração nas perguntas revisadas por stakeholder.

--- javascript/questions.js
+++ javascript/questions.js
@@ -6,7 +6,7 @@ const questions = [
         questions: [
             {
                 question: "O que caracteriza um narrador em uma história?",
-                options: ["Ele não cria os personagens.", "O narrador não é apenas um espectador.", "O narrador pode ter opinião sobre os acontecimentos.", "O narrador é quem conta a história."],
+                options: ["Ele cria os personagens.", "O narrador é apenas um espectador.", "O narrador nunca terá opinião sobre os acontecimentos", "O narrador é quem conta a história."],
                 correctAnswer: 3,
             },
             {
@@ -15,28 +15,28 @@ const questions = [
                 correctAnswer: 1,
             },
             {
-                question: "O que o enredo é considerado?",
+                question: "Como um enredo é caracterizado?",
                 options: ["Sucessão de fatos de uma história", "Sucessão de regras", "Conjunto de Fatos", "Conjunto de acontecimentos"],
                 correctAnswer: 0,
             },
             {
-                question: "Quais são os tipos principais de personagens em uma narrativa?",
-                options: ["Protagonista, antagonista, coadjuvante e figurante", "Protagonista, antagonista, coadjuvante e secundário", "Herói, vilão, anti-herói e mártir", "Personagens planos, redondos, principais e secundários"],
-                correctAnswer: 1,
+                question: "Quais os tipos de personagens mais comuns em um conto?",
+                options: ["Protagonista, antagonista, coadjuvante e figurante.", "Protagonista, antagonista, coadjuvante e secundário.", " Herói, vilão, anti-herói e mártir.", "Personagens planos, redondos, principais e secundários."],
+                correctAnswer: 0,
             },
             {
                 question: "Quais são os principais elementos que compõem uma narrativa?",
-                options: ["Faltam outros elementos importantes, como espaço e conflito.", "Texto não é um dos elementos principais.", "Diálogos não é um elemento principal, embora importante.", "Os principais elementos são personagens, enredo, espaço, tempo e conflito."],
+                options: ["Apenas espaço e conflito são os elementos principais.", "O ambiente e o clímax são os principais elementos da narrativa.", "Diálogo é o elemento principal da narrativa.", "Os principais elementos são personagens, enredo, espaço, tempo e conflito."],
                 correctAnswer: 3,
             },
             {
                 question: "Qual é a função do clímax em uma história?",
-                options: ["O clímax não apresenta os personagens.", "O clímax não resolve todos os conflitos.", "O clímax é o momento de maior tensão na história.", "O clímax não é para apresentar o cenário."],
+                options: ["Apresentar os personagens da história.", "Resolver todos os conflitos da história.", " É o momento de maior tensão na história.", "Não apresenta cenário."],
                 correctAnswer: 2,
             },
             {
-                question: "De que forma o espaço pode influenciar o desenrolar do enredo?",
-                options: ["O espaço tem impacto no enredo.", "O espaço vai além de apenas mostrar onde os personagens vivem.", "O espaço define o ambiente e afeta as ações dos personagens.", "O espaço é relevante para o desenvolvimento da trama."],
+                question: "Quais elementos compõem a estrutura do texto narrativo?",
+                options: ["Situação Inicial, desfecho, lead e explicação.", "Apresentação da situação inicial, exemplificação, clímax e moral.", "Situação Inicial, complicação, clímax e desfecho.", "Introdução, situação inicial, desenvolvimento e clímax."],
                 correctAnswer: 2,
             },
             {
